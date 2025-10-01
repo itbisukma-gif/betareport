49f5672ee2ae05aeb17bbbe78307078c9fd43571
Bisukma IT <itbisukma@gmail.com>
2025-10-01 12:49:40 +0000

    Buat background untuk header agar konten main tidak menimpa header, back

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
   const title = getTitle();
 
   return (
-    <header className="fixed top-0 left-0 w-full flex items-center justify-between p-4 bg-background/95 backdrop-blur-sm z-10">
+    <header className="fixed top-0 left-0 w-full flex items-center justify-between p-4 bg-background z-10">
       <div className="flex items-center">
         {pathname === '/' ? (
           <div className="flex items-baseline">
