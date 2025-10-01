5f6e6c56fd9eefda69eea3a8bc9e552ee7da1f9c
Bisukma IT <itbisukma@gmail.com>
2025-10-01 07:30:52 +0000

    hanya sekali saat ditekan

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-smooth': 'pulse-smooth 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'pulse-smooth': 'pulse-smooth 0.5s cubic-bezier(0.4, 0, 0.6, 1)',
       },
     },
   },
