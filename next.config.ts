9c51885389c2bf5400834886626605b541f40b3f
Bisukma IT <itbisukma@gmail.com>
2025-10-02 10:10:03 +0000

    404: NOT_FOUND Code: NOT_FOUND ID: sin1::476tf-1759399758664-bec2b6b0492

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -27,4 +29,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
